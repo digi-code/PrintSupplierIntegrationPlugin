9853de0adea636c7aa0e5d0b8b1518d2d854a5f3
pavel.novikov <pavel.novikov@benefitsystem.pl>
2023-02-22 13:28:33 +0100

    new-plugin: dto refactor

--- src/Service/CurrencyExchangeApiService/DTO/CurrencyListDTO.php
+++ src/Service/CurrencyExchangeApiService/DTO/CurrencyListDTO.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace GamesealPlugin\Service\CurrencyExchangeApiService\DTO;
 
-class CurrencyListApiDTO
+class CurrencyListDTO
 {
     public string $code;
     public float $value;
